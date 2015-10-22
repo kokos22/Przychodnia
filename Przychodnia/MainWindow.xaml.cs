2ef60abe3a77c89da2a73870decb6a600d283c4f
Zelman15 <zelman1994@gmail.com>
2015-10-23 00:59:33 +0200

    user
    Dodanie użytkownika

--- Przychodnia/MainWindow.xaml.cs
+++ Przychodnia/MainWindow.xaml.cs
@@ -93,5 +93,18 @@ namespace Przychodnia
                 }
             }
         }
+
+        //jakoś tak:
+        /*
+        tabControl1.Selecting += new TabControlCancelEventHandler(tabControl1_Selecting);
+
+        void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            TabPage current = (sender as TabControl).SelectedTab;
+            //validate the current page, to cancel the select use:
+            e.Cancel = true;
+        }
+        */
+
     }
 }
---
+++ Przychodnia/Uzytkownik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przychodnia
+{
+    static class Uzytkownik
+    {
+        static Uzytkownik()
+        {
+            Login = null;
+            PoziomDostepu = 0;
+        }
+
+        public static string Login { get; private set; }
+        public static int PoziomDostepu { get; private set; }
+        
+        public static bool SprawdzHaslo(string iLogin, string iHaslo)
+        {
+            //sprawdz haslo
+
+            //ustaw poziom dostepu
+
+            return true;
+        }
+    }
+}
